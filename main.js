351c34c95cf2331e2391da7bd11713fb4bff04cc
Chathuri Imesha <imeshachathuri6@gmail.com>
2024-03-27 06:54:35 +0530

    complete implementation

--- main.js
+++ main.js
@@ -11,3 +11,30 @@ const createWindow = () => {
   app.whenReady().then(() => {
     createWindow()
   })
+// const { app, BrowserWindow } = require('electron');
+// const path = require('path');
+// const electronReload = require('electron-reload');
+
+// function createWindow() {
+//   const win = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       nodeIntegration: true,
+//     },
+//   });
+
+//   // Load index.html file
+//   win.loadFile('index.html');
+// }
+
+// // Enable live reloading in development
+// if (process.env.NODE_ENV === 'development') {
+//   electronReload(__dirname, {
+//     electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+//     hardResetMethod: 'exit',
+//   });
+// }
+
+// // Create the window when the app is ready
+// app.whenReady().then(createWindow);
